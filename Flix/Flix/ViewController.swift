360a8add1c6f0be8181711bc6fe6bfa785f5ed19
Chao Jiang <nickjc870111@gmail.com>
2022-02-06 19:10:05 -0600

    generated tableview programmatically

--- Flix/Flix/ViewController.swift
+++ Flix/Flix/ViewController.swift
@@ -7,14 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieListViewController: UIViewController {
+    
+    var movieListTBView = UITableView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .cyan
-        // Do any additional setup after loading the view.
+//        self.view.backgroundColor = .cyan
+        
+        self.view.addSubview(movieListTBView)
+        setMovieListTBViewDelegate()
+        configureMovieListTBView()
+        
     }
+    
+    func setMovieListTBViewDelegate() {
+        self.movieListTBView.delegate = self
+        self.movieListTBView.dataSource = self
+    }
+    
+    func configureMovieListTBView() {
+        self.movieListTBView.rowHeight = 100
+        
+        self.movieListTBView.translatesAutoresizingMaskIntoConstraints = false
+        self.movieListTBView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.movieListTBView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.movieListTBView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.movieListTBView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        
+    }
+    
+    
 
 
 }
+
+extension MovieListViewController: UITableViewDelegate {}
+
+extension MovieListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+
 
